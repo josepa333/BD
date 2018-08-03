53e2e3bb386be4f97831153089e5d3cdaa60d598
Josepa <yogesh.josepa393@gmail.com>
2018-08-02 22:44:51 -0600

    dos partes

---
+++ Paises/Paises/ViewModel/Multipledata.cs
@@ -0,0 +1,17 @@
+﻿using Paises.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Paises.ViewModel
+{
+    public class Multipledata
+    {
+        public IEnumerable<pais> Paises { get; set; }
+
+        public IEnumerable<persona> Personas { get; set; }
+
+    }
+}
